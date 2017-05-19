2a384fff3a5fbe4ad312517b2518c5b1262a8fa9
Poloz Alexey <polozhev@mail.ru>
2017-05-19 22:42:50 +0300

    New script

---
+++ Assets/Spher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spher : MonoBehaviour {
+	rigidbody myRigidbody;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myRigidbody.position.y < 3) Destroy(gameObject);
+	}
+}
